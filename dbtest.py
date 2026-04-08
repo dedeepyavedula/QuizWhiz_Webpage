838750a43bbac44a1ad093ee3e52e06a768890b6
jaspreet2jas <jaspreet2jas@gmail.com>
2026-04-08 21:47:13 +0530

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -3,10 +3,21 @@ import sqlite3
 conn = sqlite3.connect('quiz.db')
 c = conn.cursor()
 
-print("ID   Name         Score")
-print("---------------------------")
+# USERS TABLE
+c.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL
+)
+''')
 
-for row in c.execute("SELECT * FROM results"):
-    print(f"{row[0]:<5}{row[1]:<13}{row[2]}")
+# RESULTS TABLE
+c.execute('''
+CREATE TABLE IF NOT EXISTS results (
+    name TEXT PRIMARY KEY,
+    score INTEGER
+)
+''')
 
+conn.commit()
 conn.close()
